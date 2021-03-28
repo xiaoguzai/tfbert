30bdd0510bb8803a26d3b06fc08b36ee89c5ccbb
xiaoguzai <31726901+boss2020@users.noreply.github.com>
2021-03-28 08:04:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tf2bert',
+    name='tfbert',
     version='1.0.0',
     description='a simple bert for beginner',
-    long_description='tf2bert:https://github.com/boss2020/tf2bert',
+    long_description='tfbert:https://github.com/boss2020/tf2bert',
     url='https://github.com/boss2020/tf2bert',
     author='xiaoguzai',
     author_email='474551240@qq.com',
